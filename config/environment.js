3524278c7c65a15a0092fce945d6d5df4da70e1f
sally pang <sallyxpang_@live.com>
2016-02-27 16:14:03 -0800

    signin

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,11 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'myapp',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://ubceventhub.firebaseio.com/',
+    torii: {
+      sessionServiceName: 'session'
+    },
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
